9e3edbc6bcac53d58131c1b6c0efbc53f7083d35
Geovanni LUcas <geovanni_lucas@yahoo.com>
2021-03-06 21:37:35 -0600

    database updated

--- src/database.js
+++ src/database.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 
-const URI = 'mongodb://localhost:27017/cars';
+const URI = 'mongodb+srv://carsuser:mbYo85JWysMwE9Qi@cluster0.zmfb3.mongodb.net/Cars?retryWrites=true&w=majority';
 
 mongoose.connect(URI)
 .then(db=>console.log('DB Connected'))
 .catch(err=>console.log(err));
+
+
+
+
 
 module.exports = mongoose;
